ba42effa7d1f5c612feff9b9fa38c6b20141bd13
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-05-07 18:32:59 +0530

    adding home page

---
+++ google-clone/src/pages/Home.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./Home.css";
+import { Link } from "react-router-dom";
+import { Avatar } from "@mui/material";
+import AppsIcon from "@mui/icons-material/Apps";
+import Search from "./Search";
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="home_header">
+        <div className="home_headerLeft">
+          {/* Link -->About*/}
+          <Link to="/about">About</Link>
+          {/* Link -->Store*/}
+          <Link to="/store">Store</Link>
+        </div>
+        <div className="home_headerRight">
+          {/* Link -->Gmail*/}
+          <Link to="/gmail">Gmail</Link>
+          {/* Link -->Images*/}
+          <Link to="/images">Images</Link>
+          {/* Icon */}
+          <AppsIcon />
+          {/* Avatar */}
+          <Avatar />
+        </div>
+      </div>
+      <div className="home_body">
+        <img
+          src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+          alt="google-logo"
+        />
+        <div className="home_inputContainer">
+          {/* Search */}
+         
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ google-clone/src/pages/Search.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import "./Search.css";
+
+const Search = () => {
+  
+  return (
+    <div className>
+     <h3>Search Component will Come Here</h3>
+    </div>
+  );
+};
+
+export default Search; 
